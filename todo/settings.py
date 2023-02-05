41bd01a748cecf1824ffc6b82f59675e146e7382
moinul75 <moinul1955198@gmail.com>
2023-02-06 04:23:04 +0600

    up to date

--- todo/settings.py
+++ todo/settings.py
@@ -83,22 +83,11 @@ WSGI_APPLICATION = 'todo.wsgi.application'
 # }
 import os
 DATABASE_URL = os.environ.get('DATABASE_URL')
+DATABASES = {'default': dj_database_url.parse(DATABASE_URL)}
 # DATABASES = {
 #     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 # }
 
-if DATABASE_URL:
-    DATABASES = {'default': dj_database_url.parse(DATABASE_URL)}
-
-# If the environment variable is not set, use the default database settings
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
-    
 
 
 
